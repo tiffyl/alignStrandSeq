2139c9d340a80a046e01276317eb455be6061b2a
tiffyl <tiffyyleung@gmail.com>
2025-07-14 16:39:58 -0700

    upgrade plots

--- scripts/10a-metrics_plots.py
+++ scripts/10a-metrics_plots.py
@@ -24,22 +24,29 @@ columns_order = ['Quality', 'Background', 'Duplication_rate', 'Complexity_at_1Gb
 metdf = pd.read_table('metrics_details.tsv')
 metdf_chiponly = metdf.query("not R.isna()").copy()
 metdf_chiponly[['R', 'C']] = metdf_chiponly[['R', 'C']].astype({'R': int, 'C': int})
-metdf_sampleonly = metdf_chiponly.query('Sample != ["negativecontrol", "empty"]')[['Sample', 'Quality']]
+metdf_chiponly['RC'] = metdf_chiponly.apply(lambda row: [row['R'], row['C']], axis=1)
+metdf_sampleonly = metdf_chiponly[~metdf_chiponly['Sample'].str.contains('negative|empty', case=False, na=False)][['Sample', 'Quality']]
+# metdf_sampleonly = metdf_chiponly.query('Sample != ["negativecontrol", "empty"]')[['Sample', 'Quality']]
 
 # Plots
 pdf = PdfPages('heatmaps.pdf')
 
 # Sample Map
-labels = metdf_chiponly.groupby('Sample').agg(R=('R', 'min'), C=('C','min'), N=('Sample', 'count')).reset_index().reset_index()
+# labels = metdf_chiponly.groupby('Sample').agg(R=('R', 'min'), C=('C','min'), N=('Sample', 'count')).reset_index().reset_index()
+labels = metdf_chiponly.groupby('Sample').agg(RC=('RC', 'min'), N=('Sample', 'count')).reset_index().reset_index()
+labels[['R', 'C']] = pd.DataFrame(labels['RC'].to_list())
 labels['Label'] = labels['Sample']  + " (" + labels['N'].astype(str) +")"
 try:
-    labels.loc[labels.query('Sample == "negativecontrol"').index[0], 'index'] = -1
-    labels.loc[labels.query('Sample == "empty"').index[0], 'index'] = -1
+    labels.loc[labels['Sample'].str.contains("negative|empty"), 'index'] = -1
 except IndexError:
     pass
 
 samplemap = metdf_chiponly[['Sample', 'R', 'C']].merge(labels[['Sample', 'index']]).pivot(index="R", columns="C", values="index")
 combined_cmap = colors.ListedColormap(np.vstack((plt.cm.tab20(np.linspace(0, 1, 20)), plt.cm.tab20b(np.linspace(0, 1, 20)), plt.cm.Set1(np.linspace(0, 1, 9)), plt.cm.Dark2(np.linspace(0, 1, 8)))))
+
+allcolumns = list(range(metdf_chiponly['C'].min(), metdf_chiponly['C'].max()))
+allrows = list(range(metdf_chiponly['R'].min(), metdf_chiponly['R'].max()))
+samplemap = samplemap.reindex(columns=allcolumns).reindex(allrows)
 
 # Metrics Heatmaps
 with PdfPages('heatmaps.pdf') as pdf:
@@ -49,8 +56,8 @@ with PdfPages('heatmaps.pdf') as pdf:
 
     for i in labels.query('index > -1').index.tolist():
         plt.text(labels.iloc[i]['C'] - min(labels['C']) + 0.5, 
-                 labels.iloc[i]['R'] - min(labels['R']) + 0.5, 
-                 labels.iloc[i]['Label'], fontsize=15, ha='left')
+                    labels.iloc[i]['R'] - min(labels['R']) + 0.5, 
+                    labels.iloc[i]['Label'], fontsize=15, ha='left')
 
     plt.xlabel("Column", fontsize=20)
     plt.ylabel("Row", fontsize=20)
@@ -64,6 +71,7 @@ with PdfPages('heatmaps.pdf') as pdf:
     for col in columns_order:
         plt.figure(figsize=(dim + 3, dim))
         matrix = metdf_chiponly.merge(labels[['Sample', 'index']]).pivot(index="R", columns="C", values=col)
+        matrix = matrix.reindex(columns=allcolumns).reindex(allrows)
         ax = sns.heatmap(matrix, mask=(matrix.isna()), cmap="viridis", vmin=0)
         
         cbar = ax.collections[0].colorbar
